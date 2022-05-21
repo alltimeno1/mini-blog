5616a4e8a340a3bddcf4f6296d7d91fabe521838
Sungjun Kim <hzcd108@gmail.com>
2022-05-21 14:02:35 +0900

    1st commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const connect = require('./schemas')
+const app = express()
+const port = 3000
+
+connect()
+
+const Router = require('./routes/goods')
+
+const requestMiddleware = (req, res, next) => {
+  console.log(req.originalUrl, new Date())
+  next()
+}
+
+app.use(express.json())
+app.use(requestMiddleware)
+
+app.use('/', Router)
+
+app.listen(port, () => {
+  console.log(port, '포트 실행')
+})
