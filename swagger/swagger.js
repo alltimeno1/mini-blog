31d584d157cce4eb1c046e604ca7ddc2eb1d9947
Sungjun Kim <hzcd108@gmail.com>
2022-06-06 11:07:50 +0900

    Swagger url 포트번호 추가

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -8,7 +8,7 @@ const options = {
       title: 'mini-blog',
       description: '간단한 블로그 기능을 포함한 웹사이트 API',
     },
-    host: '3.39.161.93',
+    host: '3.39.161.93:4000',
     basePath: '/',
   },
   apis: ['./routes/*.js', './swagger/*.yaml'], //Swagger 파일 연동
